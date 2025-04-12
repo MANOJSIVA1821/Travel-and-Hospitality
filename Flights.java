a8b4c53f86fdf7f269485a854c00dca2281973c3
siva <manojsiva1821@gmail.com>
2025-04-13 01:06:45 +0530

    Update Flights.java

--- Flights.java
+++ Flights.java
@@ -3,5 +3,7 @@ public class FlightBook
     public static void main (String[]args)
     {
       System.out.println("Flight is Booked");
+      System.out.println("pick a date");
+      System.out.println("Enter a seat num");
     }
   }
